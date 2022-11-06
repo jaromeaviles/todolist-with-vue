a5508ca22b3891df51b49b5f12e2bf2828a2f453
jaromeaviles <jaromeaviles@gmail.com>
2022-11-06 23:24:11 +0800

    Todo List with Vue build

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Todos</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <ul>
+          <li class="row align-items-center justify-content-center" v-for="todo in todos" :key="todo.id">
+            <div class="col-10 text-left">
+              {{ todo.todoName }}
+            </div>
+            <div class="col-2 close-container">
+             <i class="fa-solid fa-circle-xmark text-danger" @click="deleteTodo(todo.id)"></i>
+            </div>
+          </li>
+        </ul>
+        <add-todo @added-todo="addTodo"></add-todo>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import AddTodo from "./components/AddTodo.vue";
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      todos: [],
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    AddTodo,
+  },
+  methods: {
+    addTodo(todo) {
+      this.todos.unshift(todo);
+    },
+    deleteTodo(id) {
+      let index = this.todos.findIndex(res => {
+        return res.id === id;
+      });
+      this.todos.splice(index, 1);
+    }
+  },
+
+};
 </script>
 
 <style>
@@ -22,5 +54,37 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.container {
+  max-width: 800px;
+}
+
+h1 {
+  font-size: 60px;
+  margin-bottom: 35px;
+  font-weight: bold;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  padding: 5px 0;
+  font-size: 25px;
+  text-align: left;
+  margin: 10px 0;
+}
+.fa-solid {
+  font-size: 40px;
+  cursor: pointer;
+}
+
+.close-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
---
+++ src/components/AddTodo.vue
@@ -0,0 +1,36 @@
+<template>
+  <form @submit.prevent="addTodo()">
+    <div class="row align-items-center justify-content-center">
+      <div class="col-auto">
+        <input ref="inputValue"
+          type="text"
+         
+          class="form-control"
+          placeholder="e.g What to do?"
+        />
+      </div>
+      <div class="col-auto">
+        <button class="btn btn-success">Submit</button>
+      </div>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+    emits: ['added-todo'],
+    methods: {
+        addTodo() {
+            if (this.$refs.inputValue.value == '') {
+                return;
+            }
+            let todo = {
+                id: Math.floor(Math.random() * 1000),
+                todoName: this.$refs.inputValue.value
+            }
+            this.$emit('added-todo', todo);
+            this.$refs.inputValue.value = '';
+        }
+    }
+};
+</script>
